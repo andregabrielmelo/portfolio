106e7f6a5a02368ed706b3aba6693955a80908fd
André Melo <andregabrielmelo@gmail.com>
2025-12-06 23:55:05 -0300

    Change icons position to be more visible

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,60 +1,7 @@
-import Link from "next/link";
-
-import { buttonVariants } from "./ui/button";
-
-import { Mail } from "lucide-react";
-import { Github } from "./logos/Github";
-import { Linkedin } from "./logos/Linkedin";
-
 export default function Footer({ className }: { className?: string }) {
-    const buttonClasses = "transition-all";
-
     return (
-        <footer
-            className={className || "flex py-4 items-center max-w-5xl mx-auto"}
-        >
+        <footer className={className || "flex py-4 items-center max-w-5xl mx-auto"}>
             <p>&#169; 2025 André Melo</p>
-            <div className="flex gap-2 ml-auto">
-                <Link
-                    href="mailto:andregabrielmelo@gmail.com"
-                    className={buttonVariants({
-                        variant: "ghost",
-                        className: buttonClasses,
-                        size: "icon",
-                    })}
-                    title="E-mail"
-                >
-                    <Mail size={24} />
-                </Link>
-
-                <Link
-                    href="https://github.com/andregabrielmelo"
-                    className={buttonVariants({
-                        variant: "ghost",
-                        className: buttonClasses,
-                        size: "icon",
-                    })}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="Github"
-                >
-                    <Github />
-                </Link>
-
-                <Link
-                    href="https://www.linkedin.com/in/andre-gabriel-melo/"
-                    className={buttonVariants({
-                        variant: "ghost",
-                        className: buttonClasses,
-                        size: "icon",
-                    })}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="Linkedin"
-                >
-                    <Linkedin />
-                </Link>
-            </div>
         </footer>
     );
 }
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
 
+import { buttonVariants } from "./ui/button";
+
+import { Mail } from "lucide-react";
+import { Github } from "./logos/Github";
+import { Linkedin } from "./logos/Linkedin";
+
 import Logo from "./Logo";
 
 import { ThemeToggle } from "@/app/components/ThemeToggle";
 
 export default function Header({ className }: { className?: string }) {
+    const buttonClasses = "transition-all";
+
     return (
         <>
             <header
@@ -34,7 +42,7 @@ export default function Header({ className }: { className?: string }) {
                                 Projetos
                             </Link>
                         </li>
- <li>
+                        <li>
                             <Link
                                 className="text-muted-foreground hover:underline hover:underline-offset-4"
                                 href="/hobbies"
@@ -43,6 +51,48 @@ export default function Header({ className }: { className?: string }) {
                             </Link>
                         </li> 
                     </ul> */}
+                    <div className="flex gap-2 ml-auto">
+                        <Link
+                            href="mailto:andregabrielmelo@gmail.com"
+                            className={buttonVariants({
+                                variant: "ghost",
+                                className: buttonClasses,
+                                size: "icon",
+                            })}
+                            title="E-mail"
+                        >
+                            <Mail size={24} />
+                        </Link>
+
+                        <Link
+                            href="https://github.com/andregabrielmelo"
+                            className={buttonVariants({
+                                variant: "ghost",
+                                className: buttonClasses,
+                                size: "icon",
+                            })}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="Github"
+                        >
+                            <Github />
+                        </Link>
+
+                        <Link
+                            href="https://www.linkedin.com/in/andre-de-melo/"
+                            className={buttonVariants({
+                                variant: "ghost",
+                                className: buttonClasses,
+                                size: "icon",
+                            })}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="Linkedin"
+                        >
+                            <Linkedin />
+                        </Link>
+                    </div>
+                    <div className="h-6 w-px mx-2 bg-primary" />
                     <ThemeToggle />
                 </nav>
             </header>
